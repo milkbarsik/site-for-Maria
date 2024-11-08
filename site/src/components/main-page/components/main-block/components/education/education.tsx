689098df70a576b212ec0e83bd78df5f3277726f
RomanikhinMikhail <mihail19092005@gmail.com>
2024-11-08 23:38:34 +0300

    pages/ui-image

--- site/src/components/main-page/components/main-block/components/education/education.tsx
+++ site/src/components/main-page/components/main-block/components/education/education.tsx
@@ -1,11 +1,28 @@
 import { FC } from "react";
-
+import styles from './education.module.css';
+import Myimage from "../../../../../../assets/ui/image";
+import image from 'assets/education/diplom.jpg';
 
 const Education:FC = () => {
 
 	return (
-		<div>
-			asda
+		<div className={styles.wrapper}>
+			<h1>Образование</h1>
+			<Myimage 
+				url={image}
+				position="center"
+				size="100% 100%"
+				height="auto"
+				width="100%"
+			/>
+			<p>
+				В 2011 году закончила с отличием Московский государственный областной университет, 
+				присуждена квалификация "Филолог. Преподаватель по специальности Филология".
+			</p>
+			<p>
+				В 2014 году в Московском государственном областном университете закончила 
+				аспирантуру на кафедре стилистики русского языка, культуры речи и риторики.
+			</p>
 		</div>
 	)
 }
--- site/src/components/main-page/components/main-block/components/productivity/productivity.tsx
+++ site/src/components/main-page/components/main-block/components/productivity/productivity.tsx
@@ -1,11 +1,48 @@
 import { FC } from "react";
-
+import styles from './productivity.module.css';
+import Myimage from "../../../../../../assets/ui/image";
+import tituljpg from 'assets/productivity/titul.jpg';
+import rusTable from 'assets/productivity/rustable.png';
+import rusGraphic from 'assets/productivity/rusgraphic.png'
 
 const Productivity:FC = () => {
 
 	return (
-		<div>
-
+		<div className={styles.wrapper}>
+			<h1>Продуктивность образовательной деятельности</h1>
+			<Myimage 
+				url={tituljpg}
+				position="center"
+				size="contain"
+				height="calc((1vh + 1vw)*25)"
+				width="auto"
+			/>
+			<p>
+				Начиная с 2019-2020 учебного года, я продемонстрировала 
+				следующие результаты освоения обучающимися образовательных программ 
+				по итогам мониторингов, проводимых организацией (общеобразовательная школа): 
+			</p>
+			<p>
+				<ul>
+					<li>
+						средний уровень качества знаний по русскому языку (59%);
+							<Myimage 
+								url={rusTable}
+								position="center"
+								size="cover"
+								height="auto"
+								width="100%"
+							/>
+							<Myimage 
+								url={rusGraphic}
+								position="center"
+								size="cover"
+								height="auto"
+								width="100%"
+							/>
+					</li>
+				</ul>
+			</p>
 		</div>
 	)
 }
